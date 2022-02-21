8fe101be60e72c392530b67fa5996f35bc09735f
金野裕希 <95370823+yuukikonnobot@users.noreply.github.com>
2022-02-21 20:53:57 +0900

    Update set-date.py

--- set-date.py
+++ set-date.py
@@ -30,15 +30,15 @@ front_matter_regex = re.compile(
 # A format string for the command to get the creation date of a post file
 date_command = "TZ=UTC0 git log --date=iso-local --format='%%cd' %s | tail -n 1"
 
-for file in os.listdir(post_dir):
-    path = os.path.join(post_dir, file)
+for filename in os.listdir(post_dir):
+    filepath = os.path.join(post_dir, filename)
 
-    if os.path.isfile(path):
-        with open(path, "r") as f:
+    if os.path.isfile(filepath):
+        with open(filepath, "r") as f:
             content = f.read()
 
-        with open(path, "w") as f:
-            date = os.popen(date_command % path).read().rstrip()
+        with open(filepath, "w") as f:
+            date = os.popen(date_command % filepath).read().rstrip()
 
             # Insert the front matter at the beginning of a post content (or replace existing front matter)
             content = re.sub(front_matter_regex, front_matter % date, content)
